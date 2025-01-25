c0dae53d2b951311779651f5fdaa85cc2cbeaa29
Asif Nawaz <asifnawazshareef@gmail.com>
2025-01-25 10:23:35 +0500

    add the counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const initialValue = 0;
+  const [counter, setCounter] = useState(initialValue);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="h-screen bg-[#efeff6] w-full">
+      <h1 className="text-lg font-medium text-center py-5 bg-[#6667ab] text-white">
+        Counter App
+      </h1>
+      <div className="flex justify-center mt-10">
+        <div className="flex gap-3 mt-3">
+          <button
+            onClick={() => setCounter(counter + 1)}
+            className="bg-blue-400 px-4 py-1 rounded-md"
+          >
+            + Add Apple
+          </button>
+          <button
+            onClick={() => {
+              if (counter > 0) {
+                setCounter(counter - 1);
+              }
+            }}
+            className="bg-red-400 px-4 py-1 rounded-md"
+          >
+            - Remove Apple
+          </button>
+          <button
+            onClick={() => {
+              setCounter(initialValue);
+            }}
+            className="bg-yellow-400 px-4 py-1 rounded-md"
+          >
+            Reset
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex justify-center items-center">
+        <h2 className="bg-[#6667ab] text-white px-5 py-3 rounded-full mt-10 text-lg font-semibold">
+          {counter}
+        </h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </section>
+  );
+};
 
-export default App
+export default App;
